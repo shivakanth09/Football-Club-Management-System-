e5f94764d84ecc856dce00c0152db3ee5243a0f0
shivakanth09 <shivakanth56157@gmail.com>
2025-04-13 16:19:12 -0500

    PostgreSQL Connection Function

--- TKINTER/football_gui.py
+++ TKINTER/football_gui.py
@@ -5,3 +5,12 @@ import psycopg
 # Configure appearance
 ctk.set_appearance_mode("System")  # Options: "System", "Dark", "Light"
 ctk.set_default_color_theme("blue")  # Options: "blue", "green", "dark-blue"
+
+# Establish PostgreSQL connection
+def connect_db():
+    return psycopg.connect(
+        host="localhost",
+        dbname="Football-Club-Management-System",
+        user="postgres",
+        password="shiva2025"
+    )
